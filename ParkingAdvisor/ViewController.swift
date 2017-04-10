c9f7d113259d61e945609bb177064f27245c0493
MagicUmom <ken51006b@hotmail.com>
2017-04-10 17:28:30 +0800

    init googlemap

---
+++ ParkingAdvisor/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ParkingAdvisor
+//
+//  Created by MCLAB on 2017/4/10.
+//  Copyright © 2017年 MCLAB. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        navigationItem.title = "Hello Map"
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.868,
+                                              longitude: 151.2086,
+                                              zoom: 14)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        
+        let marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.map = mapView
+        
+        view = mapView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
