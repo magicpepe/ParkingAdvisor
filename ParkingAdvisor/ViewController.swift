ffd408015a72b6410b19d6faa47479e06fb88cd8
MagicUmom <ken51006b@hotmail.com>
2017-04-14 19:36:58 +0800

    新增地圖頁右下角的my location button
    可以獲取使用者位置、並顯示在地圖中央
    需要實機測試

--- ParkingAdvisor/ViewController.swift
+++ ParkingAdvisor/ViewController.swift
@@ -9,23 +9,58 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: BaseViewController {
+class ViewController: BaseViewController ,CLLocationManagerDelegate{
     
     @IBOutlet weak var btn_next: UIButton!
 
-//    override func loadView() {
-//    }
+    let locationManager = CLLocationManager()
+    
+    override func loadView() {
+
+        super.loadView()
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+//        let locValue:CLLocationCoordinate2D = locationManager.location!.coordinate
+//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+
+//        if CLLocationManager.authorizationStatus() == .notDetermined{
+//            locationManager.requestAlwaysAuthorization()
+//        }
+//            // 2. 用戶不同意
+//        else if CLLocationManager.authorizationStatus() == .denied {
+//            let alert = UIAlertController(title: "Alert", message: "未提供GPS資訊", preferredStyle: UIAlertControllerStyle.alert)
+//            alert.addAction(UIAlertAction(title: "瞭解", style: UIAlertActionStyle.default, handler: nil))
+//            self.present(alert, animated: true, completion: nil)
+//
+//        }
+//            // 3. 用戶已經同意
+//        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+//            locationManager.startUpdatingLocation()
+//        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "MyMap"
         
-        let camera = GMSCameraPosition.camera(withLatitude: -33.868,
-                                              longitude: 151.2086,
+        let locValue:CLLocationCoordinate2D = locationManager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+
+        
+        let camera = GMSCameraPosition.camera(withLatitude: locValue.latitude,
+                                              longitude: locValue.longitude,
                                               zoom: 14)
 //        let mapView = GMSMapView.map(withFrame: CGRect(x:0 ,y:90 , width:view.frame.width,height : view.frame.height/2), camera: camera)
         let mapView = GMSMapView.map(withFrame: CGRect(x:0 ,y:0 , width:view.frame.width,height : view.frame.height), camera: camera)
-
+        mapView.settings.myLocationButton = true
         let marker = GMSMarker()
         
         marker.position = camera.target
